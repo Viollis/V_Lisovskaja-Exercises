d7be87078d6741b278dfbe4699bffb8be33a7a3c
Viollis <151857619+Viollis@users.noreply.github.com>
2024-03-13 10:55:49 +0200

    Add files via upload

---
+++ JAVA_projet/TaxCalculatorImplementation.java
@@ -0,0 +1,11 @@
+package lt.techin.municipality.test;
+
+import lt.techin.municipality.Person;
+import lt.techin.municipality.TaxCalculator;
+
+public class TaxCalculatorImplementation implements TaxCalculator {
+    @Override
+    public double calculateTax(Person person) {
+        return 0;
+    }
+}
